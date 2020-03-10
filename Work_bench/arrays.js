23141347738ba74d05b619c25eb05bd359227720
pramodxp <pramodnitya798@gmail.com>
2020-03-10 13:01:56 +0530

    [#00001] - objects in Javascript.

---
+++ Work_bench/arrays.js
@@ -0,0 +1,35 @@
+
+/* 
+Arrays in JS
+-- arrays are dynamic in nature in JS.
+--arrays can have different data type of values.
+--arrays in java scripts are indexed.
+*/
+
+var cars = ["BMW", "Volvo", "Audi", "Lamborgini"];
+cars.push("mercedis");
+cars.push(283)
+document.write("<h1>"+cars+"</h1></br>");
+document.write("<h1>"+cars[1]+"</h1>")
+
+/*
+loop over array 
+ */
+for(var i=0; i<cars.length;i++)
+{
+    document.write("<h1>"+cars[i]+"</h1>")
+}
+
+//creating array using new keyword
+document.write("<h1>creating array using new keyword</h1></br>")
+var arr1 = new Array("hai", "hello", "how", "are", "you");
+arr1.push("mercedis");
+arr1.push(12);
+for(var i=0; i<arr1.length;i++)
+{
+    document.write("<h1>"+arr1[i]+"</h1>")
+}
+
+/* 
+--arrays can be increased and decreased dynamically in JS
+*/
---
+++ Work_bench/objectsinJs.js
@@ -0,0 +1,55 @@
+/* 
+--literal way of creating object in JS.----->similar to ananomus inner class in JAVA.
+*/
+// var car = { // this denotes the car variable is an object.
+//     car_brand :"Tesla",
+//     car_model :"model 3",
+//     price : 35000,
+
+//     //creating a function for reuse.
+//     teslaAutoPoilet : function()
+//     {
+//        return "<h1>this car is a auto poilet</h1>";
+//     }
+
+// }
+
+
+//we can use object and call the functions and use the variables.
+
+// document.writeln("<h1>"+car.car_brand+"</h1>");
+// document.writeln("<h1>"+car.teslaAutoPoilet()+"</h1>");
+
+
+/* 
+2 - creating templete  using  constructor function.
+*/
+
+function car(car_brand, car_model, price){
+    this.car_brand = car_brand;
+    this.car_model = car_model;
+    this.price = price;
+
+    this.teslaAutoPoilet = function(){
+        return "<h1>this car is a auto poilet</h1></br>"; 
+    }
+}
+
+// creating objects of cars using 
+
+var car1 = new car("hundai","2019",2000010);
+var car2 = new car("Omini", "1996", 20000);
+
+document.writeln(car1.car_brand+"</br>");
+document.writeln(car1.car_model+"</br>");
+document.writeln(car1.price+"</br>");
+
+car1.teslaAutoPoilet();
+
+//car 2 details
+
+document.writeln(car2.car_brand+"</br>");
+document.writeln(car2.car_model+"</br>");
+document.writeln(car2.price+"</br>");
+
+car2.teslaAutoPoilet();
